16cc375146408a1b0afdd4cf3e45ebdc6b1f1fc7
ngot <zhuanghengfei@gmail.com>
2014-12-19 15:46:40 +0800

    添加错误，健壮性例子

---
+++ error/fib_http_handle.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var svr = new http.Server(8080, function(r){
+	r.response.write('Hello, World!');
+	sasgdfagfasdfk;
+});
+svr.run();
---
+++ error/node_http_handle.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function(req, res) {
+	res.end('Hello, World!');
+
+	sasgdfagfasdfk
+
+}).listen(8080);
